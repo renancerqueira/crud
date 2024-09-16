c78e3e89ef7f442d718e0eade3998748e22bbcc2
Renan Cerqueira <renancerqueira@gmail.com>
2024-09-15 21:06:36 -0300

    fix dash

--- my-angular-crud/src/app/app.routes.ts
+++ my-angular-crud/src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 import { AuthGuard } from './auth/auth.guard';
 import { BooksComponent } from './books/books.component';
 
